9db95c1e5118d7d1d53b156ad93afdcaddded229
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-05-22 11:24:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,23 +4,8 @@ function threeSum(arr, target) {
 	let sum = 0;
 	let ans = 0;
 	let diff = 10000000000;
-	/*for (let i = 0; i < 3; i++) {
-		sum += arr[i];
-	}
-	diff = Math.abs(target - sum);
-	let i = 3;
-	let j = 0;
-	ans = sum;
-	while (i < arr.length) {
-		sum += arr[i++];
-		sum -= arr[j++];
-		if (diff > Math.abs(target - sum)) {
-			diff = Math.abs(target - sum);
-			ans = sum;
-		}
-	}*/
 
-	for(let i=0; i<arr.length -2; i++) {
+	/*for(let i=0; i<arr.length -2; i++) {
 		for(let j=i+1; j<arr.length-1; j++) {
 			for(let k=j+1; k<arr.length; k++) {
 				
@@ -32,6 +17,30 @@ function threeSum(arr, target) {
 					if(ans == target)
 						return ans;
 				}
+			}
+		}
+	}*///Time complexity : O(n^3);
+
+	arr.sort(function(a,b){return a-b});
+	
+	for (let i = 0; i < arr.length - 2; i++) {
+		let st = i + 1;
+		let en = arr.length - 1;
+
+		while (st < en) {
+			sum = arr[i] + arr[st] + arr[en];
+
+			if (diff > Math.abs(target - sum)) {
+				diff = Math.abs(target - sum);
+				ans = sum;
+				if (ans == target)
+					return ans;
+			}
+
+			if (sum > target)
+				en--;
+			else {
+				st++;
 			}
 		}
 	}

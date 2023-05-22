a706cd953dfca7fa7702a245e1654fb81c601416
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-05-22 10:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let sum = 0;
+	let ans = 0;
+	let diff = 10000000000;
+  for(let i=0; i<3; i++) {
+	  sum += arr[i];
+  }
+	  diff = Math.abs(target - sum);
+  let i = 3;
+	let j = 0;
+	ans = sum;
+	while(i < arr.length) {
+		sum += arr[i++];
+		sum -= arr[j++];
+		if(diff > Math.abs(target - sum)) {
+			diff = Math.abs(target - sum);
+			ans = sum;
+		}
+	}
+	return ans;
 }
 
 module.exports = threeSum;
